b623c7b94aac900f4e137db65adbb498050efeb5
carl990804 <2532848142@qq.com>
2021-02-20 11:07:52 +0800

    mall

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+import toast from 'components/common/toast'
+
+Vue.config.productionTip = false
+// 时间总线
+Vue.prototype.$bus =new Vue ()
+
+// 安装toast插件
+Vue.use(toast)
+
+
+
+
+new Vue({
+  render: h => h(App),
+  router,
+  store
+}).$mount('#app')
---
+++ src/views/category/Category.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper" ref="aaaa">
+    <div>
+      
+
+      
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Category",
+    data() {
+      return {
+        scroll: null
+      }
+    },
+    // 组件创建完后调用
+    mounted() {
+      this.scroll = new BScroll(this.$refs.aaaa, {
+        // probeType: 3,
+        // pullUpLoad: true
+      })
+
+      this.scroll.on('scroll', (position) => {
+        console.log(position);
+      })
+
+      this.scroll.on('pullingUp', () => {
+        console.log('上啦加载更多');
+      })
+    },
+    methods: {
+      btnClick() {
+        console.log('btnClick');
+      },
+      divClick() {
+        console.log('divClick');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    height: 150px;
+   
+
+    overflow: hidden;
+    /*overflow-y: scroll;*/
+  }
+</style>
